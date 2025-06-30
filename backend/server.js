f3c37fa96bdff7ec8ac07fbde2e9911f3529a5ac
HimeshLaddha <himeshladdha@gmail.com>
2025-06-30 18:25:30 +0530

    backend route fixed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/contact', require('./routes/contact'));
-app.use('/status', (req, res)=> res.send("Backend is running"));
+app.use('/', (req, res)=> res.send("Backend is running"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
